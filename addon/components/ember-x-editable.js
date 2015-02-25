11cee79acf498037976b529530495f8dfd5148d7
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-02-25 15:34:46 -0500

    Fixed height calculation

--- addon/components/ember-x-editable.js
+++ addon/components/ember-x-editable.js
@@ -16,10 +16,13 @@ export default Ember.Component.extend({
         if (!this.get('isEditing')) {
           var size = this.getTextWidth(this.$('select'), this.$('select option:selected').text());
           this.$('.selectContainer').css('width', 'auto');
+          this.$('.selectContainer').height(size.height + 8);
           this.$('select').width(size.width);
+          this.$('select').height(size.height + 7);
           this.$('.borderBottom').css('width', size.width);
         } else {
           this.$('.selectContainer').css('width', '68%');
+          this.$('.selectContainer').height('auto');
           this.$('select').css('width', '100%');
         }
       }
