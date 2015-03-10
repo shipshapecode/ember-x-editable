c80df250dbeca22b662ea6f93cad6ea666eccac7
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-10 13:43:42 -0400

    No longer include bootstrap automatically

--- index.js
+++ index.js
@@ -13,27 +13,6 @@ module.exports = {
   },
   included: function(app) {
     this._super.included(app);
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css.map', {
-      destDir: 'assets'
-    });
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-      destDir: 'fonts'
-    });
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-      destDir: 'fonts'
-    });
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-      destDir: 'fonts'
-    });
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-      destDir: 'fonts'
-    });
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
-      destDir: 'fonts'
-    });
-
-    this.app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
 
     this.app.import(app.bowerDirectory + '/bower-webfontloader/webfont.js');
 
