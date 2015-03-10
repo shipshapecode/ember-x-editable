cd93f50f790f68ec507edfaf7d6eef39a85c251e
Robert Wagner <rwwagner@Roberts-MacBook-Pro.local>
2015-03-10 16:55:03 -0400

    Adjusted border bottom, added bootstrap to Brocfile

--- Brocfile.js
+++ Brocfile.js
@@ -18,4 +18,27 @@ var app = new EmberAddon();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
+app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css.map', {
+  destDir: 'assets'
+});
+
+app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+  destDir: 'fonts'
+});
+app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+  destDir: 'fonts'
+});
+app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+  destDir: 'fonts'
+});
+app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+  destDir: 'fonts'
+});
+app.import(app.bowerDirectory + '/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+  destDir: 'fonts'
+});
+
+app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+
 module.exports = app.toTree();
