635919b173773e4cdd902087c7f7f09eb6e6873b
Robert Wagner <rwwagner90@gmail.com>
2016-05-09 17:45:00 -0400

    Split into separate tests

--- tests/acceptance/text-save-cancel-test.js
+++ tests/acceptance/text-save-cancel-test.js
@@ -3,14 +3,18 @@ import moduleForAcceptance from '../helpers/module-for-acceptance';
 
 moduleForAcceptance('Text Save/Cancel');
 
-test("changing text and clicking save/cancel correctly updates value", function (assert) {
-  assert.expect(5);
-
+test('text is initially TestString', function (assert) {
+  assert.expect(2);
   visit('/');
   andThen(function () {
     assert.equal(find('.ember-x-editable-text', 'html').hasClass('is-not-editing'), true, "is-not-editing class initially");
     assert.equal(find('.ember-x-editable-text', 'html').val(), 'TestString', "text is initially TestString");
   });
+});
+
+test('text is saved', function (assert) {
+  assert.expect(2);
+  visit('/');
   click('.ember-x-editable-text');
   andThen(function () {
     assert.equal(find('.ember-x-editable-text', 'html').hasClass('is-editing'), true, "is-editing class after clicking");
@@ -20,10 +24,15 @@ test("changing text and clicking save/cancel correctly updates value", function 
   andThen(function () {
     assert.equal(find('.ember-x-editable-text', 'html').val(), 'New test string', "text is saved");
   });
+});
+
+test("text is cancelled", function (assert) {
+  assert.expect(1);
+  visit('/');
   click('.ember-x-editable-text');
   fillIn('.ember-x-editable-text', 'Cancelled text');
   click('.editable-buttons .editable-cancel');
   andThen(function () {
-    assert.equal(find('.ember-x-editable-text', 'html').val(), 'New test string', "text cancelled");
+    assert.equal(find('.ember-x-editable-text', 'html').val(), 'TestString', "text cancelled");
   });
 });
