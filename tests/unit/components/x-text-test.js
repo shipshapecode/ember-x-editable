d823e9ecb251c51c24e4c32d187c410d2cd63442
Robert Wagner <rwwagner90@gmail.com>
2016-05-23 15:40:17 -0400

    Various tests for empty text values

--- tests/unit/components/x-text-test.js
+++ tests/unit/components/x-text-test.js
@@ -12,3 +12,47 @@ test('should set isValid based on errorMessage', function(assert) {
   xText.set('errorMessage', null);
   assert.equal(xText.get('isValid'), true);
 });
+
+test('isEmpty: not empty', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', 'Hello world');
+  assert.equal(xText.get('isEmpty'), false);
+});
+
+test('isEmpty: \'\'', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', '');
+  assert.equal(xText.get('isEmpty'), true);
+});
+
+test('isEmpty: Empty', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', 'Empty');
+  assert.equal(xText.get('isEmpty'), true);
+});
+
+test('isEmpty: null', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', null);
+  assert.equal(xText.get('isEmpty'), true);
+});
+
+test('handleEmptyValue: null', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', null);
+  xText.handleEmptyValue();
+  assert.equal(xText.get('value'), 'Empty');
+});
+
+test('handleEmptyValue: \'\'', function(assert) {
+  assert.expect(1);
+  const xText = this.subject();
+  xText.set('value', '');
+  xText.handleEmptyValue();
+  assert.equal(xText.get('value'), 'Empty');
+});
