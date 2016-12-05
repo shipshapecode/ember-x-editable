e6ee5ceada51199655dd96ed5a6231acf14a180a
Robert Wagner <rwwagner90@gmail.com>
2016-12-05 13:57:58 -0500

    Try to correct blueprints

---
+++ blueprints/ember-x-editable/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  normalizeEntityName: function() {
+    // this prevents an error when the entityName is
+    // not specified (since that doesn't actually matter
+    // to us
+  },
+
+  afterInstall: function() {
+    return this.addBowerPackagesToProject({
+      packages: [
+        {
+          name: 'bootstrap',
+          target: '~3.3'
+        },
+        {
+          name: 'bower-webfontloader',
+          target: '~1.5.13'
+        }
+      ]
+    });
+  }
+};
