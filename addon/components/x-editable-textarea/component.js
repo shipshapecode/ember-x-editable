00c3eacbe401540a80ed9d52b302f93dd6c6d69e
Ricardo Antonio Duarte <ricardo.duarte@triersistemas.com.br>
2018-03-21 14:48:30 -0300

    Add support for textarea

---
+++ addon/components/x-editable-textarea/component.js
@@ -0,0 +1,62 @@
+import { get, set } from '@ember/object';
+import { computed } from '@ember/object';
+import { observer } from '@ember/object';
+import { run } from '@ember/runloop';
+import XBaseComponent from '../x-base/component';
+import layout from './template';
+
+export default XBaseComponent.extend({
+  layout,
+
+  /**
+   * This is a computed property for adding/removing the is-empty class
+   * @private
+   */
+  isEmpty: computed('isEditing', function() {
+    if (!get(this, 'isEditing')) {
+      if (!get(this, 'value') || get(this, 'value') === '' || get(this, 'value') === 'Empty') {
+        return true;
+      }
+    }
+    return false;
+  }),
+
+  changeUnderlineSize: observer('isEditing', function() {
+    run.later(() => {
+      if (!get(this, 'isEditing')) {
+        if (get(this, 'value') && get(this, 'value').length > 0) {
+          const borderBottom = this.element.querySelector('.borderBottom');
+          const textarea = this.element.querySelector('textarea');
+          const textContainer = this.element.querySelector('.textContainer');
+          const size = this.getTextWidth(textarea, get(this, 'value'));
+          textContainer.style.width = '68%';
+          textarea.style.width = `${size.width + 10}px`;
+          borderBottom.style.width = `${size.width + 3}px`;
+        }
+      }
+    });
+  }),
+
+  makeFullWidthWhenEditing: observer('isEditing', function() {
+    this.element.querySelector('textarea').style.width = '100%';
+  }),
+
+  actions: {
+    saveAction() {
+      if (!get(this, 'validator')) {
+        this.handleEmptyValue();
+      }
+      this._super(...arguments);
+    }
+  },
+
+  /**
+   * Set the value to the string 'Empty' when value is null, undefined, or ''.
+   * @private
+   */
+  handleEmptyValue() {
+    if (!get(this, 'value') || get(this, 'value') === '') {
+      set(this, 'value', 'Empty');
+    }
+  }
+});
