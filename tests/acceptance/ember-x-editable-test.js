ead3e86692fccc84ace3481f27e7a1d62d8bc7f6
Robert Wagner <rwwagner90@gmail.com>
2018-08-08 21:37:35 -0700

    Remove unused test helpers, convert tests to new format

--- tests/acceptance/ember-x-editable-test.js
+++ tests/acceptance/ember-x-editable-test.js
@@ -1,88 +1,80 @@
 import $ from 'jquery';
-import { test } from 'qunit';
+import { module, test } from 'qunit';
+import { setupApplicationTest } from 'ember-qunit';
 import { blur, click, fillIn, find, visit } from '@ember/test-helpers';
-import moduleForAcceptance from '../helpers/module-for-acceptance';
 
-moduleForAcceptance('Acceptance | ember-x-editable');
+module('Acceptance | ember-x-editable', function(hooks) {
+  setupApplicationTest(hooks);
 
-test('text is initially TestString', async function(assert) {
-  assert.expect(2);
+  test('text is initially TestString', async function(assert) {
+    assert.expect(2);
 
-  await visit('/');
+    await visit('/');
 
-  assert.equal(find('.ember-x-editable-text').classList.contains('is-not-editing'), true, 'is-not-editing class initially');
-  assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text is initially TestString');
-});
+    assert.equal(find('.ember-x-editable-text').classList.contains('is-not-editing'), true, 'is-not-editing class initially');
+    assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text is initially TestString');
+  });
 
-test('is-editing class applied', async function(assert) {
-  assert.expect(1);
+  test('is-editing class applied', async function(assert) {
+    assert.expect(1);
 
-  await visit('/');
-  await click('.ember-x-editable-text');
+    await visit('/');
+    await click('.ember-x-editable-text');
 
-  assert.equal(find('.ember-x-editable-text').classList.contains('is-editing'), true, 'is-editing class after clicking');
-});
+    assert.equal(find('.ember-x-editable-text').classList.contains('is-editing'), true, 'is-editing class after clicking');
+  });
 
-test('text is saved', async function(assert) {
-  assert.expect(1);
+  test('text is saved', async function(assert) {
+    assert.expect(1);
 
-  await visit('/');
-  await click('.ember-x-editable-text');
-  await fillIn('.ember-x-editable-text', 'New test string');
-  $('.editable-buttons .editable-submit').click();
-  andThen(function() {
+    await visit('/');
+    await click('.ember-x-editable-text');
+    await fillIn('.ember-x-editable-text', 'New test string');
+    $('.editable-buttons .editable-submit').click();
     assert.equal(find('.ember-x-editable-text', 'html').value, 'New test string', 'text is saved');
   });
-});
 
-test('text is cancelled, cancel button', async function(assert) {
-  assert.expect(1);
+  test('text is cancelled, cancel button', async function(assert) {
+    assert.expect(1);
 
-  await visit('/');
-  await click('.ember-x-editable-text');
-  await fillIn('.ember-x-editable-text', 'Cancelled text');
-  await click('.editable-buttons .editable-cancel');
+    await visit('/');
+    await click('.ember-x-editable-text');
+    await fillIn('.ember-x-editable-text', 'Cancelled text');
+    await click('.editable-buttons .editable-cancel');
 
-  assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text cancelled');
-});
-
-test('text is cancelled, focusOut', async function(assert) {
-  assert.expect(1);
-
-  await visit('/');
-  await click('.ember-x-editable-text');
-  await fillIn('.ember-x-editable-text', 'Cancelled text');
-  await blur('.ember-x-editable-text');
-
-  assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text cancelled');
-});
-
-test('x-select save change', async function(assert) {
-  assert.expect(1);
-
-  await visit('/');
-  await click('.ember-x-editable-select');
-  $('.ember-x-editable-select option:nth-child(3)').prop('selected', true).trigger('change');
-  andThen(function() {
-    $('.editable-buttons .editable-save').click();
+    assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text cancelled');
   });
-  andThen(function() {
+
+  test('text is cancelled, focusOut', async function(assert) {
+    assert.expect(1);
+
+    await visit('/');
+    await click('.ember-x-editable-text');
+    await fillIn('.ember-x-editable-text', 'Cancelled text');
+    await blur('.ember-x-editable-text');
+
+    assert.equal(find('.ember-x-editable-text').value, 'TestString', 'text cancelled');
+  });
+
+  test('x-select save change', async function(assert) {
+    assert.expect(1);
+
+    await visit('/');
+    await click('.ember-x-editable-select');
+    $('.ember-x-editable-select option:nth-child(3)').prop('selected', true).trigger('change');
+    $('.editable-buttons .editable-save').click();
     const select = find('.ember-x-editable-select');
     assert.equal(select.options[select.selectedIndex].text, 'ReallyReallyLongTestString', 'selection saved');
   });
-});
 
-test('x-select cancel change', async function(assert) {
-  assert.expect(1);
+  test('x-select cancel change', async function(assert) {
+    assert.expect(1);
 
-  await visit('/');
+    await visit('/');
 
-  await click('.ember-x-editable-select');
-  $('.ember-x-editable-select option:nth-child(1)').prop('selected', true).trigger('change');
-  andThen(function() {
+    await click('.ember-x-editable-select');
+    $('.ember-x-editable-select option:nth-child(1)').prop('selected', true).trigger('change');
     $('.editable-buttons .editable-cancel').click();
-  });
-  andThen(function() {
     const select = find('.ember-x-editable-select');
     assert.equal(select.options[select.selectedIndex].text, 'LongerTestString', 'selection cancelled');
   });
--- tests/integration/components/ember-x-editable-test.js
+++ tests/integration/components/ember-x-editable-test.js
@@ -1,31 +1,32 @@
-import { moduleForComponent, test } from 'ember-qunit';
-import { click, fillIn, find, focus, triggerEvent } from '@ember/test-helpers';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, fillIn, find, focus, render, triggerEvent } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('ember-x-editable', 'Integration | Component | ember x editable', {
-  integration: true
-});
+module('Integration | Component | ember x editable', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('mouseEnter/mouseLeave', async function(assert) {
-  this.set('mouseInsideComponent', false);
-  this.render(hbs`{{x-editable-text mouseInsideComponent=mouseInsideComponent validator=validator value=value}}`);
-  await triggerEvent(find('.x-base'), 'mouseover');
-  assert.equal(this.get('mouseInsideComponent'), true);
-  await triggerEvent(find('.x-base'), 'mouseout');
-  assert.equal(this.get('mouseInsideComponent'), false);
-});
+  test('mouseEnter/mouseLeave', async function(assert) {
+    this.set('mouseInsideComponent', false);
+    await render(hbs`{{x-editable-text mouseInsideComponent=mouseInsideComponent validator=validator value=value}}`);
+    await triggerEvent(find('.x-base'), 'mouseover');
+    assert.equal(this.get('mouseInsideComponent'), true);
+    await triggerEvent(find('.x-base'), 'mouseout');
+    assert.equal(this.get('mouseInsideComponent'), false);
+  });
 
-test('Empty value', async function(assert) {
-  this.set('value', 'Empty');
-  this.render(hbs`{{x-editable-text validator=validator value=value}}`);
-  await focus(find('.x-base div input'));
-  assert.equal(this.get('value'), '');
-});
+  test('Empty value', async function(assert) {
+    this.set('value', 'Empty');
+    await render(hbs`{{x-editable-text validator=validator value=value}}`);
+    await focus(find('.x-base div input'));
+    assert.equal(this.get('value'), '');
+  });
 
-test('No validator passed', async function(assert) {
-  this.set('value', 'foo');
-  this.render(hbs`{{x-editable-text value=value}}`);
-  await fillIn(find('.x-base div input'), '');
-  await click('.editable-submit');
-  assert.equal(this.get('value'), 'Empty');
+  test('No validator passed', async function(assert) {
+    this.set('value', 'foo');
+    await render(hbs`{{x-editable-text value=value}}`);
+    await fillIn(find('.x-base div input'), '');
+    await click('.editable-submit');
+    assert.equal(this.get('value'), 'Empty');
+  });
 });
